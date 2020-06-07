972c172429841323cb6649bb9063dfc9cccf8a5d
Gabriel Fonseca <gabrielsqsf@gmail.com>
2020-06-07 14:35:41 -0300

    Alterações de organização para trabalhar com routes

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (request, response) => {
-    console.log("Teste")
-    response.json(['Teste1', 'Teste2'])
-});
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333)
